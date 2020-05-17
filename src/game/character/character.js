5ac7f30501433f2dcc75c99a596d80667043bcfc
Angélo LIMA <angelomiguellima@gmail.com>
2020-05-17 02:24:04 +0200

    refacto: Déplacement du personnage

--- src/game/character/character.js
+++ src/game/character/character.js
@@ -11,8 +11,7 @@ export class Character {
         this.frame = 0;
         this.offsetX = 0;
         this.offsetY = 0;
-        this.speed = 3;
-
+        this.speed = 5;
         this.page = page;
 
         subscribe(() => {
@@ -29,7 +28,7 @@ export class Character {
             this.getFrame(this.direction).y,
             13,
             16,
-            16 * this.x + this.offsetX,
+            16 * this.x + this.offsetX + 1,
             16 * this.y + this.offsetY,
             13,
             16
@@ -38,25 +37,25 @@ export class Character {
 
     checkChangePage() {
 
-        if (16 * this.x + this.offsetX < -6) {
+        if (16 * this.x + this.offsetX < - 13/2) {
             this.page = {
                 x: this.page.x,
                 y: this.page.y - 1
             }
-            this.offsetX = this.offsetX + 160
-        } else if (16 * this.x + this.offsetX > 150) {
+            this.offsetX = this.offsetX + 10 * 16;
+        } else if (16 * this.x + this.offsetX > 160 - 13/2) {
             this.page = {
                 x: this.page.x,
                 y: this.page.y + 1
             }
-            this.offsetX = this.offsetX - 160
-        } else if (16 * this.y + this.offsetY > 120) {
+            this.offsetX = this.offsetX - 10 * 16;
+        } else if (16 * this.y + this.offsetY > 120 + 16/2) {
             this.page = {
                 x: this.page.x + 1,
                 y: this.page.y
             }
-            this.offsetY = this.offsetY - 135;
-        } else if (16 * this.y + this.offsetY < -12) {
+            this.offsetY = this.offsetY - 8 * 16 - 16/2;
+        } else if (16 * this.y + this.offsetY < - 16/2) {
             this.page = {
                 x: this.page.x - 1,
                 y: this.page.y
@@ -68,7 +67,7 @@ export class Character {
     }
 
     getFrame(direction) {
-        if (this.frame === 0) {
+        if (this.frame % this.speed === 0) {
             this.frame = 1;
             return direction.first;
         } else if (this.frame === 1) {
@@ -98,6 +97,10 @@ export class Character {
             this.checkChangePage();
             break;
         }
+
+        // const nextX = Math.trunc(this.offsetX/16 - 13/2 + this.x + 8);
+        // const nextY = Math.trunc(this.offsetY/16 - 16/2 + this.y + 10);
+        // console.log((nextX - 1) + this.page.x * 8, (nextY- 1) + this.page.y * 10, this.map[(nextY- 1) + this.page.y * 10][(nextX - 1) + this.page.x * 8])
 
         return this.page;
 
